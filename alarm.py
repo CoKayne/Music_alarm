e6a04a32807803d15b4b8393800f7b9f814193a5
CoKayne <80445894+CoKayne@users.noreply.github.com>
2022-02-20 22:39:29 +0800

    data.json to settings.json + add everyday func

--- alarm.py
+++ alarm.py
@@ -3,78 +3,107 @@ import json
 from time import sleep
 from pygame import mixer
 
-with open("data.json", "r", encoding = "utf8") as jfile:
+with open("settings.json", "r", encoding = "utf8") as jfile:
     jdata = json.load(jfile)
     
-alarm_time = jdata["TIME"] #setting up alarm time
-song = jdata["SONG"] #setting up song to play
-which_day = jdata["DAY_NUM"]
-
 def is_week_day(week_number): #judge whether it is weekdays
     if week_number < 5:
         return True
     else:
         return False
+        
+alarm_time = jdata["TIME"] #setting up alarm time
+song = jdata["SONG"] #setting up song to play
+which_day = jdata["DAY_NUM"] #the key to check what day should the alarm be on
 
-cmd = input("What settings do you want to change? :\n" "1. alarm time\n" "2. song\n" "3. alarm day\n" "4. exit\n")
+now_day_cpy = datetime.datetime.today().weekday() #the number of day now copy to make everyday alarm work
 
-if cmd == str(1):
+while True:
 
-    change_time = input(str("Please enter the time you want to change to ex.10:27 : "))
-    with open("data.json", "r", encoding="utf8") as jfile:
-        jdata = json.load(jfile)
-    jdata["TIME"] = change_time
-    with open("data.json", "w", encoding="utf8") as jfile:
-        json.dump(jdata, jfile, indent=4)
-    alarm_time = jdata["TIME"]
+    cmd = input("What settings do you want to change? :\n" "(1) time\n" "(2) song\n" "(3) day\n" "(4) alarm on\n" "(5) alarm off\n")
 
-elif cmd == str(2):
+    if cmd == str(1):
 
-    change_song = input(str("Please enter the song you want to change to ex.high_on_life.mp3 : "))
-    with open("data.json", "r", encoding="utf8") as jfile:
-        jdata = json.load(jfile)
-    jdata["SONG"] = change_song
-    with open("data.json", "w", encoding="utf8") as jfile:
-        json.dump(jdata, jfile, indent=4)
-    song = jdata["SONG"]
-
-elif cmd == str(3):
-
-    change_day_cmd = input(str("1. weekdays\n" "2. weekends\n" "Please enter the day you want the alarm to be activate : \n"))
-
-    if change_day_cmd == str(1):
-
-        with open("data.json", "r", encoding="utf8") as jfile:
+        change_time = input(str("Please enter the time you want to change to ex.10:27 : "))
+        with open("settings.json", "r", encoding="utf8") as jfile:
             jdata = json.load(jfile)
-        jdata["DAY_NUM"] = True
-        with open("data.json", "w", encoding="utf8") as jfile:
+        jdata["TIME"] = change_time
+        with open("settings.json", "w", encoding="utf8") as jfile:
             json.dump(jdata, jfile, indent=4)
-        which_day = jdata["DAY_NUM"]
+        alarm_time = jdata["TIME"]
+        print("setting complete!")
 
-    elif change_day_cmd == str(2):
+    elif cmd == str(2):
 
-        with open("data.json", "r", encoding="utf8") as jfile:
+        change_song = input(str("Please enter the song you want to change to ex.high_on_life.mp3 : "))
+        with open("settings.json", "r", encoding="utf8") as jfile:
             jdata = json.load(jfile)
-        jdata["DAY_NUM"] = False
-        with open("data.json", "w", encoding="utf8") as jfile:
+        jdata["SONG"] = change_song
+        with open("settings.json", "w", encoding="utf8") as jfile:
             json.dump(jdata, jfile, indent=4)
-        which_day = jdata["DAY_NUM"]
+        song = jdata["SONG"]
+        print("setting complete!")
 
-elif cmd == str(4):
+    elif cmd == str(3):
 
-    while True:
+        change_day_cmd = input(str("(1) weekdays\n" "(2) weekends\n" "(3) everyday\n" "Please enter the day you want the alarm to be activate : \n"))
 
-        now_time = datetime.datetime.now().strftime("%H:%M") #the time now
-        now_day = datetime.datetime.today().weekday() #the number of day now
+        if change_day_cmd == str(1):
 
-        if(now_time == alarm_time and is_week_day(now_day) == which_day):
-            mixer.init()
-            mixer.music.load(song)
-            mixer.music.play()
-            while mixer.music.get_busy():  # wait for music to finish playing
-                sleep(1)
-            break
+            with open("settings.json", "r", encoding="utf8") as jfile:
+                jdata = json.load(jfile)
+            jdata["DAY_NUM"] = True
+            with open("settings.json", "w", encoding="utf8") as jfile:
+                json.dump(jdata, jfile, indent=4)
+            which_day = jdata["DAY_NUM"]
+            print("setting complete!")
 
-        else:
-            print("not yet")
-            sleep(10)
+        elif change_day_cmd == str(2):
+
+            with open("settings.json", "r", encoding="utf8") as jfile:
+                jdata = json.load(jfile)
+            jdata["DAY_NUM"] = False
+            with open("settings.json", "w", encoding="utf8") as jfile:
+                json.dump(jdata, jfile, indent=4)
+            which_day = jdata["DAY_NUM"]
+
+        elif change_day_cmd == str(3):
+
+            with open("settings.json", "r", encoding="utf8") as jfile:
+                jdata = json.load(jfile)
+            jdata["DAY_NUM"] = is_week_day(now_day_cpy)
+            with open("settings.json", "w", encoding="utf8") as jfile:
+                json.dump(jdata, jfile, indent=4)
+            which_day = jdata["DAY_NUM"]
+            print("setting complete!")
+            
+    elif cmd == str(4):
+
+        while True:
+
+            now_time = datetime.datetime.now().strftime("%H:%M") #the time now
+            now_day = datetime.datetime.today().weekday() #the number of day now
+
+            if(now_time == alarm_time and is_week_day(now_day) == which_day):
+                mixer.init()
+                mixer.music.load(song)
+                mixer.music.play()
+                while mixer.music.get_busy():  # wait for music to finish playing
+                    sleep(1)
+                break
+
+            else:
+                print("not yet")
+                sleep(10)
+
+    elif cmd == str(5):
+        break
+
+    # testing use
+    elif cmd == str(6):
+        print(is_week_day(now_day))
+        print(now_day)
+        print(which_day)
+        print(now_time)
+        print(alarm_time)
+        print(can_play)
--- webmToMp3.py
+++ webmToMp3.py
@@ -2,6 +2,7 @@ from pydub import AudioSegment
 
 song_name = input(str("Please enter the name of your song ex.The Chainsmokers - High (Official Lyric Video) : "))
 song = AudioSegment.from_file(song_name + ".webm")
+# TODO fix not only webm file format problem
 print("Loaded")
 song.export("neco.mp3", format="mp3", bitrate="320k")
 print("Converted and saved")
